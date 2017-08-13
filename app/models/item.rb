02c4a24136eb5cf22942033dd0d7108a303e5462
Prakash <prakashpoudel@lftechnology.com>
2017-08-13 16:37:35 +0545

    ratings

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :category
+  has_many :ratings
+
+  def display_ratings
+    rating_values = ratings.collect(&:value)
+    return 0 if rating_values.count < 1
+    rating_values.inject {|sum, el| sum + el}.to_f / rating_values.size
+  end
 end
--- app/views/items/_item.json.jbuilder
+++ app/views/items/_item.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! item, :id, :name, :price, :image_url, :category_id, :created_at, :updated_at
 json.url item_url(item, format: :json)
+json.ratings item.display_ratings
