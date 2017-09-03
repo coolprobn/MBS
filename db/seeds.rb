c6060cb535bfc1e43e773688698e1a3d790317c4
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-09-03 19:41:28 +0545

    seed 1 user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@ category_pasteries = Category.create(name: 'Pasteries')
 category_cookies = Category.create(name: 'Cookies')
 
 mocha_cake = Item.create(name: 'Mocha Cake', price: 120, image_url: '/uploads/items/cakes/mocha-caramal-cake.jpg', category_id: category_cake.id)
+
+User.create(email: "user1@mandala.com", password: "mandala123", password_confirmation: "mandala123")
